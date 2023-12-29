0450cf6831780507b4c3f0ff382adb6b343dbc53
Abdellah Sabry <asabry@fullsoon.co>
2023-12-29 15:43:31 +0100

    bugfix: trying with nodes, but keep it for later

--- road_accidents/utils.py
+++ road_accidents/utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 from road_accidents.config import (
     VEHICULE_COLS,
     VEHICULE_PATH,
+    CARACTERISTIQUES_PATH,
     info_vehicules,
     obstacle_labels,
     obstacle_fixe_labels,
@@ -16,3 +17,17 @@ def read_vehicules():
     vehicules["label_obstacle_mobile"] = vehicules.obstacle_mobile.map(obstacle_labels)
     vehicules["label_obstacle_fixe"] = vehicules.obstacle_fixe.map(obstacle_fixe_labels)
     return vehicules
+
+
+def read_carateristiques():
+    """Read carateristiques from the web"""
+    caracteristiques = pd.read_csv(CARACTERISTIQUES_PATH, encoding="utf-8", sep=";")
+    caracteristiques["long"] = (
+        caracteristiques["long"].str.replace(",", ".").astype(float)
+    )
+    caracteristiques["lat"] = (
+        caracteristiques["lat"].str.replace(",", ".").astype(float)
+    )
+    caracteristiques["Num_Acc"] = caracteristiques["Accident_Id"]
+    caracteristiques.drop("Accident_Id", axis=1, inplace=True)
+    return caracteristiques
